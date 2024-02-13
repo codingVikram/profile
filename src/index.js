281f6a8d7047037e1210a4eb98337ab459e3c9bc
codingVikram <multani_v@hotmail.com>
2024-02-13 13:25:38 +0000

    basic profile and skill list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src="vm-profile-image.jpeg"
+      alt="Vikramjeet Multani"
+    ></img>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Vikramjeet Multani</h1>
+      <p>
+        Cloud Engineer with Azure and AWS DevOps skills, Currently learning full
+        stack web development using React and Java at front and backend. I like
+        watching Cricket, Rugby and Formula One.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="✌️" color="#FF5733" />
+      <Skill skill="JavaScript" emoji="✌️" color="#87D51D" />
+      <Skill skill="AWS & Azure" emoji="💪" color="#1D55D5" />
+      <Skill skill="DevOps" emoji="💪" color="#B00BDC" />
+      <Skill skill="Terraform" emoji="💪" color="#00A8BF" />
+      <Skill skill="HTML+CSS" emoji="👶" color="#DF08CC" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  console.log(props);
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
